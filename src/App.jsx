cc5bf17407d4b8bf6755e75345e13b8fd80b703e
mercy-mwai <mercyneemam@gmail.com>
2024-10-23 13:00:12 +0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const Button =(props)=>{
   )
 }
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -16,20 +15,19 @@ const App = () => {
 
 
   const handleGoodClick = () => {
-    /*setGood(good + 1);*/
     const updatedGood= good +1;
     setGood(updatedGood)
     setTotal(updatedGood + neutral + bad)
 
   };
+
   const handleNeutralClick=()=>{
-    /*setNeutral(neutral + 1)*/
     const updatedNeutral=neutral + 1
     setNeutral(updatedNeutral)
     setTotal(updatedNeutral +good + bad)
   };
+
   const handleBadClick=()=>{
-    /*setBad(bad + 1)*/
     const updatedBad=bad +1
     setBad(updatedBad)
     setTotal(updatedBad+ good + neutral)
