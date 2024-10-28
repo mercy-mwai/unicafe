51f949c0b2e3ba63d8d1f563443ebdcb120ef396
mercy-mwai <mercyneemam@gmail.com>
2024-10-28 09:03:53 +0300

    added a button showing a random anecdote

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,26 @@ const Statistics= (props) => {
 };
 
 const App = () => {
+
+const anecdotes=[
+ 'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+    "Good code is its own best documentation. As you're about to add a comment, ask yourself, 'How can I improve the code so that this comment isn't needed?' Improve the code and then document it to make it even clearer."
+
+]
+
+const displayRandomAnecdote = () => {
+  const randomIndex = Math.floor(Math.random() * anecdotes.length);
+  setSelected(randomIndex);
+};
+
+  const [selected,setSelected]=useState(0)
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -64,6 +84,8 @@ const App = () => {
       <Button handleClick={handleBadClick} text="bad" />
       <p>Statistics</p>
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <p>{anecdotes[selected]}</p>
+      <button onClick={displayRandomAnecdote}>Show random anecdote</button>
     </div>
   );
 };
